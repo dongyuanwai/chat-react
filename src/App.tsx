9cc0da29046db195cf5051de7d429b15846e340f
dongyuanwai <yuanwaidong@gmail.com>
2023-02-18 23:32:09 +0800

    feat: init样式

---
+++ src/App.tsx
@@ -0,0 +1,113 @@
+import { useRef, useState } from 'react'
+import './App.less'
+import { Button ,Spin} from 'antd';
+import { AudioOutlined ,RedditOutlined} from '@ant-design/icons';
+import 'antd/dist/reset.css';
+
+import  { Configuration, OpenAIApi } from "openai";
+interface answerListType{
+	type: string,
+	content?: string,
+	isFinshed: number,
+}
+function App() {
+  	const [inputValue, setInputValue] = useState<string>("")
+	const [answerList, setAnswerList] = useState<answerListType[]>([])
+	const preItem = useRef<HTMLDivElement>(null)
+
+	const quize = ()=>{
+		console.log("提问",inputValue)
+		setAnswerList([...answerList,{
+			type:'question',
+			content:inputValue,
+			isFinshed: 1,
+		},{
+			type: 'answer',
+			content: '正在思考',
+			isFinshed: 0,
+		}])
+		console.log("resf",preItem)
+		preItem?.current?.scrollIntoView()
+		setInputValue("")
+		getMessage(inputValue)
+	}
+
+	const configuration = new Configuration({
+		apiKey: "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",
+	});
+	const openai = new OpenAIApi(configuration);
+	const getMessage = async (questionText:string)=>{
+		openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: questionText,
+			max_tokens: 600,
+			temperature: 1,
+		}).then((response  )=>{
+
+		// 打印 API 返回的结果
+		console.log(response,response.data.choices[0],answerList);
+
+		setAnswerList(answerList=>answerList.map((item,index)=>{
+				if(index<answerList.length-1){
+					return item
+				}else{
+					return {
+						type: 'answer',
+						content: response?.data?.choices[0]?.text?.replace(/\n{2}/, " "),
+						isFinshed: 1,
+					}
+				}
+			}))
+		})
+	}
+
+  return (
+    <div className="App">
+		<div className='input_box'>
+			<input type="text" className='input' value={inputValue} onChange={e=>setInputValue(e.target.value)}></input>
+			<Button 
+				type="primary" 
+				size='large' 
+				className='btn'
+				onClick={quize}
+			>提问</Button>
+		</div>
+		<div className='answer_box'>
+			<div className='answer'>{
+				answerList.map((item,index)=>{
+					if(item.type==='question'){
+						// 问题
+						return (
+							<div className='item' key={index}>
+								<div><AudioOutlined style={{fontSize:'24px',color:'#646cffaa'}}/> </div>
+								<span className='span'>{item.content}</span>
+							</div>
+						)
+					}else{
+						// 回答
+						if(item.isFinshed === 0){
+							// 加载中
+							return (
+								<div className='item' ref={preItem} key={index}>
+									<div><RedditOutlined style={{fontSize:'24px',color:'#42b883aa'}}/> </div>
+									<span className='span'><Spin /></span>
+								</div>
+							)
+						}else{
+							// 回复答案
+							return (
+								<div className='item' key={index}>
+									<div><RedditOutlined style={{fontSize:'24px',color:'#42b883aa'}}/> </div> 
+									<span className='span'>{item.content}</span>
+								</div>
+							)
+						}
+					}
+				})
+			}</div>
+		</div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'dashesOnly'
+    },
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      }
+    }
+  },
+})
